394cb04b6bc2dddb71fce0f74011627bad966628
박민수 <minsoo1003@gmail.com>
2013-10-15 16:56:06 +0900

    moved touch event into view class

--- src/bbarm/viewtest/view/ArcView.java
+++ src/bbarm/viewtest/view/ArcView.java
@@ -6,27 +6,33 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.RectF;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
-public class ArcView extends View{
+public class ArcView extends View {
 
-	Paint paint;
-	Path path;
-	Canvas arcCanvas;
-	RectF arcOval = new RectF();
-	int color;
-	float width;
-	float left, right, top, bottom;
-	float startAngle, endAngle;
+    public interface OnAngleChangedListener {
+        public void onChanging(float angle);
+        public void onChanged(float angle);
+    }
+
+    private OnAngleChangedListener listener;
+	private Paint paint;
+    private Canvas arcCanvas;
+    private RectF arcOval = new RectF();
+    private int color;
+    private float width;
+    private float startAngle, endAngle;
+    private boolean flipping;
+    private int flipUnit;
 	
 	public ArcView(Context context) {
-		super(context);
-		init();
+		this(context, null);
 	}
 
 	public ArcView(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		init();
+		this(context, attrs, 0);
 	}
 
 	public ArcView(Context context, AttributeSet attrs, int defStyle) {
@@ -42,12 +48,18 @@ public class ArcView extends View{
 	}
 
 	public void setColor(int color) {
+        this.color = color;
 		paint.setColor(color);
 	}
 
-	public void setWidth(float width) {
+	public void setStrokeWidth(float width) {
+        this.width = width;
 		paint.setStrokeWidth(width);
 	}
+
+    public float getStrokeWidth() {
+        return this.width;
+    }
 
 	public void setOvalSize(float left, float top, float right, float bottom) {
 		arcOval.set(left, top, right, bottom);
@@ -56,8 +68,7 @@ public class ArcView extends View{
 	public void setStartAngle(float startAngle) {
 		this.startAngle = startAngle;
 	}
-	
-	
+
 	public Canvas getCanvas() {
 		return arcCanvas;
 	}
@@ -77,7 +88,27 @@ public class ArcView extends View{
 	public Paint getPaint() {
 		return paint;
 	}
-	
+
+    public void setFlippingEnabled(boolean flipping) {
+        this.flipping = flipping;
+    }
+
+    public void setFlippingUnit(int unit) {
+        this.flipUnit = unit;
+    }
+
+    public boolean isFlipping() {
+        return this.flipping;
+    }
+
+    public int getFlippingUnit() {
+        return this.flipUnit;
+    }
+
+    public void setOnAngleChangedListener(OnAngleChangedListener listener) {
+        this.listener = listener;
+    }
+
 	@Override
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
@@ -91,4 +122,59 @@ public class ArcView extends View{
 		
 	}
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+            case MotionEvent.ACTION_MOVE:
+                float x = event.getX();
+                float y = event.getY();
+                float centerX = getWidth() / 2;
+                float centerY = getHeight() / 2;
+
+                float relativeX = x - centerX;
+                float relativeY = y - centerY;
+
+                double cos = relativeX
+                        / Math.sqrt(Math.pow(relativeX, 2)
+                        + Math.pow(relativeY, 2));
+                double rad = Math.acos(cos);
+                rad = relativeY > 0 ? rad : Math.PI * 2 - rad;
+                double angle = rad * 180 / Math.PI;
+
+                setEndAngle((float) angle);
+                invalidate();
+
+                if(listener != null) {
+                    listener.onChanging((float) angle);
+                }
+
+                break;
+
+            case MotionEvent.ACTION_UP:
+                setStrokeWidth(width);
+                float touchUpAngle = getEndAngle();
+                float targetAngle = touchUpAngle;
+
+                if(flipping) {
+                    float flipToAngle = touchUpAngle - touchUpAngle % 30;
+
+                    if(touchUpAngle % 30 >= 15) {
+                        flipToAngle = flipToAngle + 30;
+                    }
+
+                    targetAngle = flipToAngle;
+                }
+
+                setEndAngle(targetAngle);
+                invalidate();
+
+                if(listener != null) {
+                    listener.onChanged(targetAngle);
+                }
+
+                break;
+        }
+        return true;
+    }
 }
